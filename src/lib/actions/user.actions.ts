e4d4b47f23d18629f2d9e81d5e57a576760a77e0
Sharoon-Masih <sharoonmasih43@gmail.com>
2024-06-04 18:09:11 +0500

    Update user.actions.ts

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -1,10 +1,9 @@
 "use server"
-
 import { revalidatePath } from 'next/cache'
 import { connectToDb } from "../database";
-import Event from "@/lib/database/models/event.model";
-import User from "@/lib/database/models/user.model";
-import Order from "../database/models/order.model";
+import EventModel from "@/lib/database/models/event.model";
+import UserModel from "@/lib/database/models/user.model";
+import OrderModel from "../database/models/order.model";
 import { CreateUserParams, UpdateUserParams } from "../types";
 import { handleError } from "../utils";
 
@@ -14,7 +13,7 @@ export async function createUser(user: CreateUserParams) {
     try {
 
         await connectToDb(); //yaha humna uss function ko call kia jo humna database connection krwanay ka liya bnaya tha, ab yeh function srf tab hi call hoga jab yeh server action call hoga, or usi ki base pa waha humna condition be lgyi hai yeh check krnay kay liya kay agr connection hua toh new conn hojaye warna previous conn return hoja.
-        const newUser = await User.create(user); //yaha simply ek new variable declare kia or usme "UserModel" jo basic humna model bnaya tha na usme create ka operation perform krnay ka liya ".create()" ka method use krka "user" create kr rhay hain. and in return wo ek promise be return kr rha hai jisme wo document hoga jo Db may create hoga or usko hum await ka through resolve krka further apna frontend pa use kreinga.
+        const newUser = await UserModel.create(user); //yaha simply ek new variable declare kia or usme "UserModel" jo basic humna model bnaya tha na usme create ka operation perform krnay ka liya ".create()" ka method use krka "user" create kr rhay hain. and in return wo ek promise be return kr rha hai jisme wo document hoga jo Db may create hoga or usko hum await ka through resolve krka further apna frontend pa use kreinga.
 
         return JSON.parse(JSON.stringify(newUser)) //actually yaha yeh JSON.parse() iss liya use kia hai taka jo return ma output milay wo pure JS object ho na kay mongoDB doc joka JSON obj hota hai wo ho, and then uska andar "JSON.stringify(newUser)" iss lia kiya hai taka pehla jo be data ayee "newUser" may wo pehla JSON obj ma convert ho through ".stringify" method and then wo wo JSON obj .parse ka through JS obj ma convert ho, bcuz later on we dont know what type of data can come in newUser toh .parse toh srf JSON ko convert krta hai toh iss lia pehla newUser ko jSON ma convert kia and then .parse ko pass kia. 
 
@@ -32,7 +31,8 @@ export async function updateUser(clerkId: string, user: UpdateUserParams) {
     try {
 
         await connectToDb();
-        const updatedUser = await User.findOneAndUpdate({ clerkId }, user, { new: true }); //yaha par by using "findOneAndUpdate" method usme jo 1st parameter hai usko clerkId ki base pa user ka doc ko find krega and then usme "user" ka jo updated obj mila wo pass krdia.
+
+        const updatedUser = await UserModel.findOneAndUpdate({ clerkId }, user, { new: true }); //yaha par by using "findOneAndUpdate" method usme jo 1st parameter hai usko clerkId ki base pa user ka doc ko find krega and then usme "user" ka jo updated obj mila wo pass krdia.
 
         //{new:true} //iska mtlb haka bydefault "findOneAndUpdate" ka method document ko return update honay sa pehlay but when we set new to true, so its mean kay update honay kay bd return kro doc ko.
 
@@ -56,28 +56,30 @@ export async function deleteUser(clerkId: string) {
 
         await connectToDb();
         //find user by clerkId field
-        const userToDelete = await User.findOne({ clerkId })
-        if (!userToDelete) throw new Error("user not found")
+        const userToDelete = await UserModel.findOne({ clerkId })
+
+        if (!userToDelete) {
+          throw new Error("user not found")}
 
 
         // Unlink relationships
         await Promise.all([
             // Update the 'events' collection to remove references to the user
-            Event.updateMany(
+            EventModel.updateMany(
                 { _id: { $in: userToDelete.events } },
                 { $pull: { organizer: userToDelete._id } }
             ),
 
             // Update the 'orders' collection to remove references to the user
-            Order.updateMany({ _id: { $in: userToDelete.orders } }, { $unset: { buyer: 1 } }),
+            OrderModel.updateMany({ _id: { $in: userToDelete.orders } }, { $unset: { buyer: 1 } }),
         ])
 
 
 
         //delete user by _id, yaha iss tarah sa hoga kay jo be "userToDelete" doc ayega usme _id hogi toh uski base pa user ko delete kreinga.
-        const deletedUser = await User.findById(userToDelete._id)
+        const deletedUser = await UserModel.findByIdAndDelete(userToDelete._id)
 
-        revalidatePath('/')
+    revalidatePath('/')
 
         return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null
 
@@ -95,10 +97,11 @@ export async function getUser(userId: string) { //ab jo yeh wala action hna yeh 
     try {
 
         await connectToDb();
-        const getUser = await User.findById(userId) //yaha par .findById or findOne ma yeh difference haka findOne jo be doc ma field detay hain uska A/c filter kta hai or jo findById hai wo jo doc ki "_id" hoti hai joka tab bnti hai jab doc create hota hai toh uska A/c filter krta hai.
+        const User = await UserModel.findById( userId ) //yaha par .findById or findOne ma yeh difference haka findOne jo be doc ma field detay hain uska A/c filter kta hai or jo findById hai wo jo doc ki "_id" hoti hai joka tab bnti hai jab doc create hota hai toh uska A/c filter krta hai.
 
-        if (!getUser) throw new Error("user not found")
-        return JSON.parse(JSON.stringify(getUser))
+        if (!User) throw new Error("user not found")
+          
+        return JSON.parse(JSON.stringify(User))
 
     }
     catch (error) {
